b9b819f020900b6dbbc79488886a4eb6eb53e85d
Juptian <juptianraffles@gmail.com>
2021-03-10 18:35:00 -0500

    It works on my machine
    It works on my machineTM

--- ROTX/Program.cs
+++ ROTX/Program.cs
@@ -17,64 +17,53 @@ namespace ROTX
         public static string Shift(char toShift, int shiftAmount)
         {
             char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
-            char result = new char();
-            int a = Array.IndexOf(alphabet, Convert.ToChar(toShift.ToString().ToLower()));
             
-            if ((a - shiftAmount) < 0) {
-                Console.WriteLine(alphabet[a + shiftAmount]);
-                result = alphabet[a + shiftAmount];
-            } 
+            int indexOf = Array.IndexOf( alphabet, Convert.ToChar( toShift.ToString().ToLower() ) );
+            char result = new char();
+            
+            while(shiftAmount >= 26)
+            {
+                shiftAmount -= 26;
+            }
+
+            if ((indexOf - shiftAmount) < 0)
+            {
+                result = alphabet[indexOf - shiftAmount + 20];
+            }
             else
             {
-                result = alphabet[a - shiftAmount];
+                result = alphabet[indexOf - shiftAmount];
             }
-
-            //hehe I'm dumb ignore this
-            /*Console.WriteLine(toShift.ToString().ToUpper());
-            int convertIndex = Array.IndexOf(alphabet, toShift.ToString().ToUpper());
-
-            if (convertIndex - shiftAmount < 0)
-            {
-                Console.WriteLine($"{convertIndex}, ${shiftAmount}");
-                result = alphabet[convertIndex + Math.Abs(convertIndex - shiftAmount)];
-            } else
-            {
-                result = alphabet[convertIndex - shiftAmount];
-            }
-
-            if(toShift.ToString().ToLower() == toShift.ToString().ToLower())
-            {
-                result = result.ToLower();
-            }*/
-            if (toShift.ToString() == toShift.ToString().ToUpper())
+            if ( toShift.ToString() == toShift.ToString().ToUpper() )
                 return result.ToString().ToUpper();
             else
-                return result.ToString();
+                return result.ToString().ToLower();
         }
+
         public static bool GameLoop()
         {
             Console.WriteLine("Input the string you would like to convert");
             char[] input = Console.ReadLine().ToCharArray();
 
             Console.WriteLine("Input the value you would like to shift your input by");
-            shiftAmount = Convert.ToInt32(Console.ReadLine());
+            shiftAmount = Convert.ToInt32( Console.ReadLine() );
 
-            Console.WriteLine(shiftAmount);
 
             string[] outputArr = new string[input.Length];
             for (int i = 0; i < input.Length; i++)
             {
                 outputArr[i] = Shift(input[i], shiftAmount);
             }
+            string output = String.Join("", outputArr);
+            Console.WriteLine($"The output is: {output}");
 
-            Console.WriteLine("The output is: ");
 
-            foreach (string s in outputArr)
+            /*foreach (string s in outputArr)
             {
                 Console.Write($"{s}");
-            }
-
-            Console.WriteLine("Would you like to go again? (y/n)");
+            }*/
+            Console.WriteLine();
+            Console.Write("Would you like to go again? (y/n): ");
             string goAgain = Console.ReadLine();
             while(true)
             {
@@ -84,7 +73,8 @@ namespace ROTX
                 } else if (goAgain.ToUpper() == "NO" || goAgain.ToUpper() == "N")
                 {
                     return false;
-                } else
+                } 
+                else
                 {
                     Console.WriteLine("Please write either y, n, or yes, no.");
                     Console.WriteLine("Case is being ignored");
