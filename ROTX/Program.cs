d8be374d275acce9e679f8d7157913e77c02a04c
Juptian <juptianraffles@gmail.com>
2021-03-11 18:22:08 -0500

    Small changes

--- ROTX/Program.cs
+++ ROTX/Program.cs
@@ -25,16 +25,6 @@ namespace ROTX
             char result;
 
             shiftAmount %= 26;
-#if false
-            if ( (indexOf - shiftAmount) < 0)
-            {
-                result = alphabet[20 + (indexOf - shiftAmount) ];
-            } 
-            else
-            {
-                result = alphabet[indexOf - shiftAmount];
-            }
-#endif
 
             if( indexOf + shiftAmount < 26)
             {
@@ -52,39 +42,26 @@ namespace ROTX
         {
             Console.WriteLine("Input the string you would like to convert");
             Console.WriteLine("abcdefghijklmnopqrstuvwxyz");
-            char[] input = Console.ReadLine().ToCharArray();
+            string input = Console.ReadLine();
 
             Console.WriteLine("Input the value you would like to shift your input by");
-            shiftAmount = Convert.ToInt32( Console.ReadLine() );
+            int.TryParse( Console.ReadLine(), out shiftAmount );
 
 
             char[] outputArr = new char[input.Length];
+            
             for (int i = 0; i < input.Length; i++)
             {
                 outputArr[i] = Shift(input[i], shiftAmount);
             }
-            string output = String.Join("", outputArr);
+
+            string output = String.Concat(outputArr);
             Console.WriteLine($"The output is: {output}");
             
             Console.WriteLine();
             Console.Write("Would you like to go again? (y/n): ");
             string goAgain = Console.ReadLine();
-            while(true)
-            {
-                if (goAgain.ToUpper() == "YES" || goAgain.ToUpper() == "Y")
-                {
-                    return true;
-                } else if (goAgain.ToUpper() == "NO" || goAgain.ToUpper() == "N")
-                {
-                    return false;
-                } 
-                else
-                {
-                    Console.WriteLine("Please write either y, n, or yes, no.");
-                    Console.WriteLine("Case is being ignored");
-                    goAgain = Console.ReadLine();
-                }
-            }
+            return goAgain.Equals("YES", StringComparison.OrdinalIgnoreCase) || goAgain.Equals("Y", StringComparison.OrdinalIgnoreCase);
         }
     }
 }
